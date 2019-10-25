934d6afde0c7681ad34ea9c4d05419c71d47caf6
Tipable <andres.ramirezp4@gmail.com>
2019-10-25 18:42:12 -0400

    add material components and created basic structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MatCardModule,
+    MatIconModule,
     MatButtonModule,
     BrowserModule,
     AppRoutingModule,
